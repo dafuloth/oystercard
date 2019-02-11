5797c4e1291b399fe72a4a6930c4fd99ccf81c8f
David Thai <davidthai@dafuloth.me>
2019-02-11 16:00:44 +0000

    amended method top_up to be aware of limit. Made rubocop happy.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 class Oystercard
   attr_reader :balance
+  MAXIMUM_LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    raise "Top-up aborted. Would exceed #{MAXIMUM_LIMIT} limit." if (@balance + amount) > MAXIMUM_LIMIT
+
     @balance += amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,14 +1,21 @@
 require_relative '../lib/oystercard'
 
 describe Oystercard do
-  it 'has a balance of 0 when newly initialized' do
-    card = Oystercard.new
-    expect(card.balance).to eq 0
-  end
 
-  it 'When top up the ammount is added to the balance' do
-    card = Oystercard.new
-    current_balance = card.balance
-    expect{card.top_up(100)}.to change{card.balance}.by 100
+  describe 'top-up' do
+    it 'has a balance of 0 when newly initialized' do
+      card = Oystercard.new
+      expect(card.balance).to eq 0
+    end
+
+    it 'When top up the ammount is added to the balance' do
+      card = Oystercard.new
+      expect { card.top_up(90) }.to change{ card.balance }.by 90
+    end
+
+    it 'cannot be topped up above the limit' do
+      card = Oystercard.new
+      expect { card.top_up(900) }.to raise_error "Top-up aborted. Would exceed #{Oystercard::MAXIMUM_LIMIT} limit."
+    end
   end
 end
